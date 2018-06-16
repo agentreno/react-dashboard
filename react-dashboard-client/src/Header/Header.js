87e1dde6e9435be5645b98540ea4d88e7c9ba1c7
Karl <karl.turrell@buto.tv>
2018-06-16 19:53:08 +0100

    Tidy up styles

--- react-dashboard-client/src/Header/Header.js
+++ react-dashboard-client/src/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+import './Header.css'
 import { Link } from 'react-router-dom'
 
 const Header = () => (
-    <header>
+    <header className="Header">
         <ul>
             <li><Link to='/posts'>Posts</Link></li>
             <li><Link to='/about'>About</Link></li>
